8f855c6d66a53b9b798e4f3696f004ed14ef85e3
Arbaaz <khanarbaj45@gmail.com>
2020-11-30 11:14:08 +0530

    Feat : Detect faces ROI And Draw Rectangle

--- main.py
+++ main.py
@@ -19,7 +19,13 @@ while True:
     # Convert color image into grayscale
     gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # Detect faces ROI
+    # Syntax: Classifier.detectMultiScale(input image, Scale Factor , Min Neighbors)
+    eyes = eye_detect.detectMultiScale(gray_img, 1.3, 7)
 
+    # Draw rectangle around the faces
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
 
 
