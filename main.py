dad42de1a8a40a4f23b16de99b8b43ceba44d0ed
Arbaaz <khanarbaj45@gmail.com>
2020-11-30 11:12:53 +0530

    Feat : Read Frame and Convert color image into grayscale

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ eye_detect = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # start camera / read video
 cam = cv2.VideoCapture('Face.mp4')
+
+while True:
+
+    # read frame/image from camera
+    resp, frame = cam.read()
+
+    # no frame then brak loop
+    if resp == 0:
+        break
+
+    # Convert color image into grayscale
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+
+
+
